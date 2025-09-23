d59d4316df30ac729d4ad07b23420f3a1bc39d64
Niall Ó Colmáin <ocolmain.niall.92@protonmail.com>
2025-09-23 13:02:56 +0100

    Made the strings WAY nicer.

--- Lab1/src/Main/Demo/Week3Demo.java
+++ Lab1/src/Main/Demo/Week3Demo.java
@@ -9,6 +9,7 @@ import Main.Domain.Order;
 import Main.Domain.OrderIds;
 import Main.Payment.CardPayment;
 import Main.Payment.CashPayment;
+import Main.Payment.WalletPayment;
 
 public class Week3Demo
 {
@@ -22,14 +23,21 @@ public class Week3Demo
         Order order1 = new Order(OrderIds.next());
         order1.addItem(new LineItem(catalog.findById("P-ESP").orElseThrow(), 2));
         order1.addItem(new LineItem(catalog.findById("P-CCK").orElseThrow(), 1));
-        System.out.println("Order #" + order1.id() + " Total: " + order1.totalWithTax(10));
+        System.out.printf("\nOrder #%s \nTotal: %s\n", order1.id(), order1.totalWithTax(10));
         order1.pay(new CashPayment());
 
         // Card payment
         Order order2 = new Order(OrderIds.next());
         order2.addItem(new LineItem(catalog.findById("P-ESP").orElseThrow(), 2));
         order2.addItem(new LineItem(catalog.findById("P-CCK").orElseThrow(), 1));
-        System.out.println("Order #" + order2.id() + " Total: " + order2.totalWithTax(10));
+        System.out.printf("\nOrder #%s \nTotal: %s\n", order2.id(), order2.totalWithTax(10));
         order2.pay(new CardPayment("1234567812341234"));
+
+        //Wallet payment
+        Order order3 = new Order(OrderIds.next());
+        order3.addItem(new LineItem(catalog.findById("P-ESP").orElseThrow(), 2));
+        order3.addItem(new LineItem(catalog.findById("P-CCK").orElseThrow(), 1));
+        System.out.printf("\nOrder #%s \nTotal: %s\n", order3.id(), order3.totalWithTax(10));
+        order3.pay(new WalletPayment("Neil&Jan-Joint-Financial-Account"));
     }
 }
