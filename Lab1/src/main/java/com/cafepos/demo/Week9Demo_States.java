f7a8eda6e3fa671fdf85e13a136fbbdae4941df8
CubeByte <businesscube.world@gmail.com>
2025-11-04 14:55:21 +0000

    Implemented the state machine for order process
    added demos for menu and state machine and implemented the state machine.

---
+++ Lab1/src/main/java/com/cafepos/demo/Week9Demo_States.java
@@ -0,0 +1,26 @@
+package main.java.com.cafepos.demo;
+
+import main.java.com.cafepos.state.OrderFSM;
+
+public final class Week9Demo_States
+{
+    public static void main(String[] args)
+    {
+        OrderFSM fsm = new OrderFSM();
+
+        System.out.println("Status = " + fsm.status());
+
+        // invalid before pay
+        fsm.prepare();
+        // NEW -> PREPARING
+        fsm.pay();
+        // still preparing
+        fsm.prepare();
+        // PREPARING -> READY
+        fsm.markReady();
+        // READY -> DELIVERED
+        fsm.deliver();
+
+        System.out.println("Status = " + fsm.status());
+    }
+}
